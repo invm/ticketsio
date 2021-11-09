7f4dc080964a2fa11d05f2dce7079477662cb1bf
invm <imichaelionov@gmail.com>
2021-11-09 08:29:22 +0200

    feat: create base publisher and some type definitions

---
+++ nats-test/src/events/subjects.ts
@@ -0,0 +1,7 @@
+export enum Subjects {
+  TicketCreated = "ticket:created",
+  TicketUpdated = "ticket:updated",
+  OrderCreated = "order:created",
+  ExpirationComplete = "expiration:complete",
+  PaymentCreated = "payment:created",
+}
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,25 +1,26 @@
-import { randomBytes } from 'crypto';
-import nats from 'node-nats-streaming';
+import { randomBytes } from "crypto";
+import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 
-const client = nats.connect('ticketsio', 'abc', {
-	url: 'http://localhost:4222'
+const client = nats.connect("ticketsio", "abc", {
+  url: "http://localhost:4222",
 });
 
-client.on('connect', () => {
-	console.log('publisher connected to nats');
-
-	const data = JSON.stringify({
-		id: randomBytes(4).toString('hex'),
-		title: randomBytes(4).toString('hex'),
-		price: Math.floor(Math.random() * 100)
-	});
-
-	client.publish('ticket:created', data, () => {
-		console.log('event published');
-	});
+client.on("connect", async () => {
+  console.log("publisher connected to nats");
+  const publisher = new TicketCreatedPublisher(client);
+  try {
+    await publisher.publish({
+      id: randomBytes(4).toString("hex"),
+      title: randomBytes(4).toString("hex"),
+      price: Math.floor(Math.random() * 100),
+    });
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-process.on('SIGINT', () => client.close());
-process.on('SIGTERM', () => client.close());
+process.on("SIGINT", () => client.close());
+process.on("SIGTERM", () => client.close());
