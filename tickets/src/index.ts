63a41fc71d438157f0157d19318ab8fee7d581e6
invm <imichaelionov@gmail.com>
2021-11-10 08:27:29 +0200

    feat: add first ticket events

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,21 +1,36 @@
-import mongoose from 'mongoose';
-import { app } from './app';
-import { DatabaseConnectionError } from '@invmtickets/common';
+import mongoose from "mongoose";
+import { app } from "./app";
+import { DatabaseConnectionError } from "@invmtickets/common";
+import { natsWrapper } from "./nats-wrapper";
+import { randomBytes } from 'crypto';
 
 const start = async () => {
-	if (!process.env.JWT_KEY || !process.env.MONGO_URI) {
-		throw new Error('Environment variables are missing');
-	}
-	try {
-		await mongoose.connect(process.env.MONGO_URI);
-		console.log('Connected to db');
-	} catch (error) {
-		console.log(error);
-		throw new DatabaseConnectionError();
-	}
-	app.listen(3000, () => {
-		console.log('Listening on port 3000!');
-	});
+  if (!process.env.JWT_KEY || !process.env.MONGO_URI) {
+    throw new Error("Environment variables are missing");
+  }
+  try {
+    await natsWrapper.connect(
+      "ticketsio",
+      randomBytes(4).toString("hex"),
+      "http://nats-srv:4222"
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("Nats connection closed");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to db");
+  } catch (error) {
+    console.log(error);
+    throw new DatabaseConnectionError();
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!");
+  });
 };
 
 start();
