79010f5313e542fcfba77970ef076709fa73ab49
invm <imichaelionov@gmail.com>
2021-11-21 22:50:57 +0200

    feat: add optimistic concurrency control in tickets and orders services

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,68 +1,64 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response } from 'express';
 import {
-  NotAuthorizedError,
-  NotFoundError,
-  requireAuth,
-  validateRequest,
-} from "@invmtickets/common";
-import { body, param } from "express-validator";
-import { Ticket } from "../models/ticket";
-import { TicketUpdatedPublisher } from "../events/publishers/TicketUpdatedPublisher";
-import { natsWrapper } from "../nats-wrapper";
+	NotAuthorizedError,
+	NotFoundError,
+	requireAuth,
+	validateRequest,
+} from '@invmtickets/common';
+import { body, param } from 'express-validator';
+import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/TicketUpdatedPublisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.patch(
-  "/:id",
-  requireAuth,
-  [
-    param("id")
-      .not()
-      .isEmpty()
-      .isMongoId()
-      .withMessage("Provide valid ticket id"),
-    body("title")
-      .not()
-      .isEmpty()
-      .isLength({ max: 200, min: 1 })
-      .withMessage("Title must not be empty"),
-    body("price")
-      .isFloat({ gt: 0 })
-      .withMessage("Price must be greater than 0"),
-  ],
-  validateRequest,
-  async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { price, title } = req.body;
+	'/:id',
+	requireAuth,
+	[
+		param('id')
+			.not()
+			.isEmpty()
+			.isMongoId()
+			.withMessage('Provide valid ticket id'),
+		body('title')
+			.not()
+			.isEmpty()
+			.isLength({ max: 200, min: 1 })
+			.withMessage('Title must not be empty'),
+		body('price')
+			.isFloat({ gt: 0 })
+			.withMessage('Price must be greater than 0'),
+	],
+	validateRequest,
+	async (req: Request, res: Response) => {
+		const { id } = req.params;
+		const { price, title } = req.body;
 
-    let ticket = await Ticket.findOne({ _id: id });
+		let ticket = await Ticket.findOne({ _id: id });
 
-    if (!ticket) throw new NotFoundError();
+		if (!ticket) throw new NotFoundError();
 
-    if (ticket.userId !== req.currentUser!.id) throw new NotAuthorizedError();
+		if (ticket.userId !== req.currentUser!.id) throw new NotAuthorizedError();
 
-    await Ticket.updateOne(
-      { _id: id },
-      {
-        $set: {
-          ...(price && { price }),
-          ...(title && { title }),
-        },
-      }
-    );
+		ticket.set({
+			...(price && { price }),
+			...(title && { title }),
+		});
 
-    ticket = await Ticket.findOne({ _id: id });
+		await ticket.save();
 
-    if (ticket)
-      await new TicketUpdatedPublisher(natsWrapper.client).publish({
-        id: ticket.id,
-        title: ticket.title,
-        price: ticket.price,
-        userId: ticket.userId,
-      });
+		if (ticket)
+			await new TicketUpdatedPublisher(natsWrapper.client).publish({
+				id: ticket.id,
+				title: ticket.title,
+				price: ticket.price,
+				userId: ticket.userId,
+				version: ticket.version,
+			});
 
-    res.status(200).send({ data: ticket });
-  }
+		res.status(200).send({ data: ticket });
+	}
 );
 
 export { router as updateTicketRoute };
