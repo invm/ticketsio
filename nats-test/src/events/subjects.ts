35dd940003f13975506779d61023d2ba9f236e02
invm <imichaelionov@gmail.com>
2022-01-03 08:33:33 +0200

    chore: cleanup

--- nats-test/src/events/subjects.ts
+++
@@ -1,7 +0,0 @@
-export enum Subjects {
-  TicketCreated = "ticket:created",
-  TicketUpdated = "ticket:updated",
-  OrderCreated = "order:created",
-  ExpirationComplete = "expiration:complete",
-  PaymentCreated = "payment:created",
-}
